085712621da6589121a6476d930b5587d8af55b3
Caudrelier <johancaudrelier@gmail.com>
2025-11-26 09:55:49 +0100

    sauvegarde

--- sauvegarde.bat
+++ sauvegarde.bat
@@ -1,6 +1,6 @@
-cd c:\Users\johan\Documents\xampp\techsolutions\bdd
-c:\Users\johan\Documents\xampp\bin\mysql dump u-root techsolutions>techsolutions.sql
-cd c:\Users\johan\Documents\xampp\htdocs\techsolutions
+cd C:\Users\johan\Documents\xampp\htdocs\techsolutions\bdd
+C:\Users\johan\Documents\xampp\bin\mysql dump u-root techsolutions>techsolutions.sql
+cd C:\Users\johan\Documents\xampp\htdocs\techsolutions
 git add .
 git commit -m "sauvegarde"
 git push origin main 
