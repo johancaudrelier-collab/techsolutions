9411f56c17457efbefc5dfa93eea6ac08e4dbef5
Caudrelier <johancaudrelier@gmail.com>
2025-11-26 09:56:57 +0100

    sauvegarde

--- sauvegarde.bat
+++ sauvegarde.bat
@@ -1,5 +1,5 @@
 cd C:\Users\johan\Documents\xampp\htdocs\techsolutions\bdd
-C:\Users\johan\Documents\xampp\bin\mysql dump u-root techsolutions>techsolutions.sql
+C:\Users\johan\Documents\xampp\bin\mysql dump -uroot techsolutions>techsolutions.sql
 cd C:\Users\johan\Documents\xampp\htdocs\techsolutions
 git add .
 git commit -m "sauvegarde"
